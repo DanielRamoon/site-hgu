aa5fdec2076be29e6096a4fe99d52f7ca173351f
Daniel Ramon <danielramoon@gmail.com>
2024-09-09 16:12:19 -0300

    ajuste-slider

--- src/components/cardService/styles.ts
+++ src/components/cardService/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const CardsContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   gap: 0.5rem;
   padding: 5rem;
   margin-top: 2rem;

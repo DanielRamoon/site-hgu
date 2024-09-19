8f39b9e54ea67c749f644bd891be3196d76533af
Daniel Ramon <danielramoon@gmail.com>
2024-09-19 13:59:35 -0300

    ajuste-telass

--- src/components/sectionPlanos/styles.ts
+++ src/components/sectionPlanos/styles.ts
@@ -97,7 +97,7 @@ export const SideImageContainer = styled.div`
 
   @media (max-width: 768px) {
     position: relative;
-    top: 4.6rem;
+    top: 4rem;
     left: 2rem;
     z-index: -1;
     text-align: center;
